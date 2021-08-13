03f85556ce33da31544cb0f9b5eb4f26cb3865d2
thiagoribb <thiribeiro142@gmail.com>
2021-08-13 16:29:57 -0300

    Preparando revisão de código

--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import * as controller from "../controllers/userController";
-import { Router } from "express";
-
-const router = Router();
-
-router.post("/sign-up", controller.signup);
-router.post("/sign-in", controller.signin);
-
-export { router as userRouter};
